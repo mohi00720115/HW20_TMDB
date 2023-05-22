c4f8f1cbfc0da2975869c124b73e7db5e6416bd7
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-05-22 18:54:32 +0430

    Add Data binding in Activity

--- app/src/main/java/com/example/hw20/MainActivity.kt
+++ app/src/main/java/com/example/hw20/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.hw20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.hw20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
