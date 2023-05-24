bd3c8e5a2771f811d3f620670d1d6c021a6c6cc6
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-05-24 21:52:47 +0430

    Change package names and ReCreate Classes.

--- app/src/main/java/com/example/hw20/data/remote/TmdbService.kt
+++ app/src/main/java/com/example/hw20/data/remote/TmdbService.kt
@@ -1,16 +1,20 @@
 package com.example.hw20.data.remote
 
+import com.example.hw20.data.model.remote.popular_mmovies_dto.PopularMoviesResponse
+import com.example.hw20.data.model.remote.top_rated_movies_dto.TopRatedMovie
+import com.example.hw20.data.model.remote.top_rated_movies_dto.TopRatedMoviesResponse
+import com.example.hw20.data.model.remote.upcoming_movies_dto.UpcomingMoviesResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TmdbService {
 
     @GET("movie/top_rated")
-    suspend fun getTopRatedMovies(@Query("page") page: Int = 1)
+    suspend fun getTopRatedMovies(@Query("page") page: Int = 1) : TopRatedMoviesResponse
 
     @GET("movie/popular")
-    suspend fun getPopularMovies(@Query("page") page: Int = 1)
+    suspend fun getPopularMovies(@Query("page") page: Int = 1) : PopularMoviesResponse
 
     @GET("movie/upcoming")
-    suspend fun getUpComingMovies(@Query("page") page: Int = 1)
+    suspend fun getUpComingMovies(@Query("page") page: Int = 1) : UpcomingMoviesResponse
 }
