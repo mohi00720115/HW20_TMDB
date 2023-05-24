afe9838f7050decb6bb9ec26828291c4d3f0516c
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-05-24 21:53:25 +0430

    Change package names and ReCreate Classes.

--- app/src/main/java/com/example/hw20/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/hw20/ui/favorite/FavoriteFragment.kt
@@ -1,16 +1,17 @@
-package com.example.hw20.ui.Search
+package com.example.hw20.ui.favorite
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.hw20.R
-import com.example.hw20.databinding.FragmentSearchBinding
+import com.example.hw20.databinding.FragmentFavoriteBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class SearchFragment : Fragment(R.layout.fragment_search) {
-    private lateinit var binding: FragmentSearchBinding
+class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+    private lateinit var binding: FragmentFavoriteBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
--- app/src/main/java/com/example/hw20/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/hw20/ui/search/SearchFragment.kt
@@ -1,17 +1,16 @@
-package com.example.hw20.ui.Favorite
+package com.example.hw20.ui.search
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.hw20.R
-import com.example.hw20.databinding.FragmentFavoriteBinding
+import com.example.hw20.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
-class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
-    private lateinit var binding: FragmentFavoriteBinding
+class SearchFragment : Fragment(R.layout.fragment_search) {
+    private lateinit var binding: FragmentSearchBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
