f0e45e00f577458c1ed19a7d7b036633e31c1fdc
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-05-24 15:28:54 +0430

    Create Constants

---
+++ app/src/main/java/com/example/hw20/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.hw20.utils
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjU1NTBjNWVmMmMyMDcxNTAyYzA0YTkzYTVmMWQ0OSIsInN1YiI6IjY0NTUwMjYwZDQ4Y2VlMDBlMTM0NDU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qyycUysBM6G7diQCq0of0uEBqA4wLEi6OtZK5XuCQ6Q"
+    const val LANGUAGE = "en-US"
+}
