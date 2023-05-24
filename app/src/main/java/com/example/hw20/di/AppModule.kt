ff2b7c4f34c79b5444bf81087125a0ad50093161
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-05-24 16:03:59 +0430

    Create Module.

--- app/src/main/java/com/example/hw20/di/AppModule.kt
+++ app/src/main/java/com/example/hw20/di/AppModule.kt
@@ -1,4 +1,79 @@
 package com.example.hw20.di
 
+import com.example.hw20.data.remote.TmdbService
+import com.example.hw20.utils.Constants.API_KEY
+import com.example.hw20.utils.Constants.BASE_URL
+import com.example.hw20.utils.Constants.LANGUAGE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val url = chain.request()
+                .url
+                .newBuilder()
+                .addQueryParameter("language", LANGUAGE)
+                .build()
+
+            val request = chain.request()
+                .newBuilder()
+                .url(url)
+                .addHeader("Authorization", API_KEY)
+                .build()
+            chain.proceed(request)
+        }
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoginInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
+
+    @Provides
+    @Singleton
+    fun provideClient(
+        interceptor: Interceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideService(retrofit: Retrofit): TmdbService {
+        return retrofit.create(TmdbService::class.java)
+    }
+
 }
