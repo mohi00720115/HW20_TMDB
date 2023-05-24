ddbff772535c59035b1be8610b640739b8d76ae1
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-05-24 21:54:17 +0430

    Calling functions from the repository in ViewModels

--- app/src/main/java/com/example/hw20/ui/Home/HomeFragment.kt
+++ app/src/main/java/com/example/hw20/ui/Home/HomeFragment.kt
@@ -1,9 +1,10 @@
-package com.example.hw20.ui.Home
+package com.example.hw20.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.hw20.R
 import com.example.hw20.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,9 +12,29 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
         binding.lifecycleOwner = viewLifecycleOwner
+//        binding.homeViewModel = viewModel
+
+        fun getTopRatedMovies() {
+            viewModel.topRatedMovie.observe(viewLifecycleOwner) {
+                viewModel.getTopRatedMovies(2)
+            }
+        }
+
+        fun getPopularMovies() {
+            viewModel.popularMovie.observe(viewLifecycleOwner) {
+                viewModel.getPopularMovies(3)
+            }
+        }
+
+        fun getUpComingMovies() {
+            viewModel.upComingMovie.observe(viewLifecycleOwner) {
+                viewModel.getUpComingMovies(4)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/hw20/ui/Home/HomeViewModel.kt
+++ app/src/main/java/com/example/hw20/ui/Home/HomeViewModel.kt
@@ -1,4 +1,41 @@
-package com.example.hw20.ui.Home
+package com.example.hw20.ui.home
 
-class HomeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hw20.data.model.remote.popular_mmovies_dto.PopularMovie
+import com.example.hw20.data.model.remote.top_rated_movies_dto.TopRatedMovie
+import com.example.hw20.data.model.remote.upcoming_movies_dto.UpcomingMovie
+import com.example.hw20.data.repository.Repository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+    private val _topRateMovie = MutableLiveData<List<TopRatedMovie>>()
+    val topRatedMovie: LiveData<List<TopRatedMovie>> = _topRateMovie
+
+    private val _popularMovie = MutableLiveData<List<PopularMovie>>()
+    val popularMovie: LiveData<List<PopularMovie>> = _popularMovie
+
+    private val _upComingMovie = MutableLiveData<List<UpcomingMovie>>()
+    val upComingMovie: LiveData<List<UpcomingMovie>> = _upComingMovie
+    fun getTopRatedMovies(page: Int) {
+        viewModelScope.launch {
+            _topRateMovie.postValue(repository.getTopRatedMovies(page).topRatedMovies)
+        }
+    }
+
+    fun getPopularMovies(page: Int) {
+        viewModelScope.launch {
+            _popularMovie.postValue(repository.getPopularMovies(page).popularMovies)
+        }
+    }
+
+    fun getUpComingMovies(page: Int) {
+        viewModelScope.launch {
+            _upComingMovie.postValue(repository.getUpComingMovies(page).upcomingMovies)
+        }
+    }
 }
